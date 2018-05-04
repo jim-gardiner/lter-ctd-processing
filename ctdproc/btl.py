92d60571d1f898f9c356b00c6c08c146e6571c26
Joe Futrelle <joefutrelle@gmail.com>
2018-05-04 14:49:00 -0400

    refactored into parent classes for common parsing tasks

--- ctdproc/btl.py
+++ ctdproc/btl.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 
+from .parsing import CtdTextParser
+
 def _col_values(line, col_widths):
     """read fixed with column values that are assumed
     to be right-justified"""
@@ -75,17 +77,94 @@ def parse_btl_file(path):
     # all done
     return df
 
-class BtlFile(object):
-    def __init__(self, path):
-        self.path = path
+class BtlFile(CtdTextParser):
+    def __init__(self, path, parse=True):
+        super(BtlFile, self).__init__(path, parse)
+        self._df = None
     def to_dataframe(self):
-        df = parse_btl_file(self.path)
+        if self._df is not None:
+            return self._df
+
+        # read lines of file, skipping headers
+        lines = []
+
+        for l in self._lines:
+            if l.startswith('#') or l.startswith('*'):
+                continue
+            lines.append(l)
+
+        # column headers are fixed with at 11 characters per column,
+        # except the first two
+        h1_width = 10
+        h2_width = 12
+
+        n_cols = ((len(lines[0]) - (h1_width + h2_width)) // 11) + 2
+
+        header_col_widths = [h1_width,h2_width] + [11] * (n_cols - 2)
+
+        # the first line is the first line of column headers; skip the second
+        col_headers = _col_values(lines[0], header_col_widths)
+
+        # discard the header lines, the rest are data lines
+        lines = lines[2:]
+
+        # average values are every four lines
+        avg_lines = lines[::4]
+        # the lines with the time (and stddev values) are the ones immediately
+        # following the average value lines
+        time_lines = lines[1::4]
+
+        # value columns are fixed width 11 characters per col except the first two
+        v1_width = 7
+        v2_width = 15
+
+        col_widths = [7,15] + [11] * (n_cols - 2)
+
+        # now assemble the rows of the dataframe
+        rows = []
+
+        for al, tl in zip(avg_lines, time_lines):
+            cvs = _col_values(al, col_widths)
+            time = _col_values(tl, col_widths)[1] # just use the first value
+            cvs[1] = '{} {}'.format(cvs[1], time)
+            rows.append(cvs)
+
+        df = pd.DataFrame(rows, columns=col_headers)
+
+        # convert df columns to reasonable types
+        df.Bottle = df.Bottle.astype(int)
+        df.Date = pd.to_datetime(df.Date)
+
+        for c in df.columns[2:]:
+            df[c] = df[c].astype(float)
+        
+        self._df = df
+
+        # all done
         return df
+
+    def niskin_time(self, niskin_number):
+        df = self.to_dataframe()
+        sdf = df[df.Bottle == int(niskin_number)]
+        return sdf.Date.iloc[0]
+
+    def niskin_depth(self, niskin_number):
+        # TODO
+        # find pressure variable
+        # convert variable units as necessary
+        # pass latitude and pressure to p_to_z
+        return niskin_number # fake value for now
 
 if __name__ == '__main__':
     import sys
     path = sys.argv[1]
     outpath = sys.argv[2]
     btl = BtlFile(path)
+    print('Cruise: {}'.format(btl.cruise))
+    print('Cast: {}'.format(btl.cast))
+    print('Lat/lon: {}/{}'.format(btl.lat, btl.lon))
+    print('Time: {}'.format(btl.time))
     df = btl.to_dataframe()
     df.to_csv(outpath, index=False)
+    for bn in df.Bottle:
+        print(bn, btl.niskin_time(bn))
--- ctdproc/hdr.py
+++ ctdproc/hdr.py
@@ -1,25 +1,16 @@
 import re
+import os
+
 import pandas as pd
 
-def parse_lat_lon(ll):
-    """convert deg/dec minutes into dec"""
-    REGEX = r'(\d+)[^\d]+([\d.]+)[^NSEW]*([NSEW])'
-    deg, mnts, hemi = re.match(REGEX, ll).groups()
-    mult = 1 if hemi in ['N', 'E'] else -1
-    deg, mnts = int(deg), float(mnts)
-    deg = mult * (deg + (mnts / 60))
-    return deg
+from .parsing import CtdTextParser
 
-class HdrFile(object):
+class HdrFile(CtdTextParser):
     def __init__(self, path, parse=True):
-        self.path = path
-        if parse:
-            self.parse()
+        super(HdrFile, self).__init__(path, parse)
     def parse(self):
-        self._read_lines()
+        super(HdrFile, self).parse()
         self._parse_names()
-        self._parse_lat_lon()
-        self._parse_time()
     def _read_lines(self):
         self.lines = []
         with open(self.path) as fin:
@@ -29,7 +20,7 @@ class HdrFile(object):
     def _parse_names(self):
         REGEX = r'# name \d+ = ([^:]+): ([^\[]+)(?:\[(.*)\](, .*)?)?'
         names, defns, units, params = set(), {}, {}, {}
-        for line in self.lines:
+        for line in self._lines:
             if not re.search(r'^# name \d+', line):
                 continue
             name, defn, unit, param = re.match(REGEX, line).groups()
@@ -44,20 +35,7 @@ class HdrFile(object):
         self.definitions = defns
         self.units = units
         self._params = params
-    def _line_that_matches(self, regex):
-        for line in self.lines:
-            if re.match(regex, line):
-                return line
-    def _parse_lat_lon(self):
-        lat_line = self._line_that_matches(r'\* NMEA Latitude')
-        lon_line = self._line_that_matches(r'\* NMEA Longitude')
-        split_regex = r'.*itude = (.*)'
-        self.lat = parse_lat_lon(re.match(split_regex, lat_line).group(1))
-        self.lon = parse_lat_lon(re.match(split_regex, lon_line).group(1))
-    def _parse_time(self):
-        line = self._line_that_matches(r'\* NMEA UTC \(Time\)')
-        time = re.match(r'.*= (.*)', line).group(1)
-        self.time = pd.to_datetime(time)
+    ## accessors
     def definition(self, name):
         return self.definitions[name]
     def units(self, name):
@@ -67,4 +45,7 @@ if __name__=='__main__':
     import sys
     hdr_path = sys.argv[1]
     hdr = HdrFile(hdr_path)
-    print(hdr.lat, hdr.lon, hdr.time)
+    print('Cruise: {}'.format(hdr.cruise))
+    print('Cast: {}'.format(hdr.cast))
+    print('Lat/lon: {}/{}'.format(hdr.lat, hdr.lon))
+    print('Time: {}'.format(hdr.time))
